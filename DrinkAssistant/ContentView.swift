db0c7f5e80a6d889326f464bfe617c2eb90bc28d
WincerChan <WincerChan@gmail.com>
2020-03-27 17:51:00 +0100

    :tada: Initial Commit

---
+++ DrinkAssistant/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DrinkAssistant
+//
+//  Created by Wincer Chan on 2020/5/12.
+//  Copyright © 2020 Wincer Chan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
