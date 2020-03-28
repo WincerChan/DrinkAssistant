b83e78f69f2c8e00203c5bf21231014d8a5b36a4
WincerChan <WincerChan@gmail.com>
2020-03-28 19:09:00 +0100

    :wrench: Add weak connections

--- DrinkAssistant/AppDelegate.swift
+++ DrinkAssistant/AppDelegate.swift
@@ -10,9 +10,10 @@ import Cocoa
 import SwiftUI
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDelegate {
 
     @IBOutlet weak var menu: NSMenu!
+    //    var window: NSWindow!
     let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
     let popover = NSPopover()
     var eventMonitor: EventMonitor?
@@ -21,14 +22,76 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-
+        if let button = statusItem.button {
+          button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+            button.action = #selector(mouseClickHandler)
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
+        }
+        popover.contentViewController = PopoverViewController.freshController()
+        eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
+          if let strongSelf = self, strongSelf.popover.isShown {
+            strongSelf.closePopover(sender: event)
+          }
+        }
+        menu.delegate = self
+        Config.loadConfig()
+        control.doResume()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
+    
+    @objc func togglePopover(_ sender: Any?) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
+    }
+    
+    @objc func mouseClickHandler() {
+        if let event = NSApp.currentEvent {
+            switch event.type {
+            case .leftMouseUp:
+                togglePopover(popover)
+            default:
+                statusItem.menu = menu
+                statusItem.button?.performClick(nil)
+            }
+        }
+    }
+    
+    func showPopover(sender: Any?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+        }
+        eventMonitor?.start()
+    }
+    
+    func closePopover(sender: Any?) {
+        popover.performClose(sender)
+        eventMonitor?.stop()
+    }
+    
+    func userNotificationCenter(_ center: NSUserNotificationCenter, shouldPresent notification: NSUserNotification) -> Bool {
+        return true
+    }
 
-
+    @IBAction func quit(_ sender: Any) {
+        NSApplication.shared.terminate(self)
+    }
+    @IBAction func pause(_ sender: Any) {
+        control.doPause()
+    }
+    
+    @IBAction func resume(_ sender: Any) {
+        control.doResume()
+    }
 }
 
+extension AppDelegate: NSMenuDelegate {
+    func menuDidClose(_ menu: NSMenu) {
+        self.statusItem.menu = nil
+    }
+}
--- DrinkAssistant/PopoverViewController.swift
+++ DrinkAssistant/PopoverViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import Cocoa
+import SwiftUI
 
-class PopoverViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do view setup here.
+struct Config {
+    static var interval: UInt32 = 600
+    static var titleString: String = "提醒喝水小助手"
+    static var subtitleString: String = "该喝水啦～"
+    static var running: Bool = false
+    static var insideloop: Bool = false
+    static var userDefaults = UserDefaults.standard
+    
+    static func loadConfig() {
+        let interval = UInt32(Config.userDefaults.integer(forKey: "interval") )
+        Config.interval = interval == 0 ? 600 : interval
+        Config.subtitleString = Config.userDefaults.string(forKey: "subtileString") ?? "该喝水啦～"
+        Config.titleString = Config.userDefaults.string(forKey: "titleString") ?? "提醒喝水小助手"
     }
     
+    static func saveConfig() {
+        Config.userDefaults.set(Config.subtitleString, forKey: "subtitleString")
+        Config.userDefaults.set(Config.titleString, forKey: "titleString")
+        Config.userDefaults.set(Config.interval, forKey: "interval")
+        Config.userDefaults.synchronize()
+    }
+}
+
+
+class PopoverViewController: NSViewController, NSUserNotificationCenterDelegate {
+    
+    @IBOutlet var popover: NSView!
+    @IBOutlet weak var maintitle: NSTextField!
+    
+    @IBOutlet weak var subtitle: NSClipView!
+    
+    @IBOutlet weak var notify: NSComboBox!
+    
+    @IBOutlet weak var submit: NSButton!
 }
